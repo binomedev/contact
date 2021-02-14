f5684e774f8476adc1940deb51e012f71d7e7684
Codrin Axinte <codrin.axinte@outlook.com>
2021-02-14 22:03:37 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,18 @@ use Binomedev\Contact\Http\Controllers\ContactController;
 use Binomedev\Contact\Http\Controllers\SubscriberController;
 use Illuminate\Support\Facades\Route;
 
-// Contact Form
-Route::get('/contact', [ContactController::class, 'index'])->name('contact.index');
-Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
+Route::group(['middleware' => ['web']], function () {
 
-if(app()->environment() === 'local'){
-    Route::get('/mailable', [ContactController::class, 'show'])->middleware('auth');
-}
+// Contact Form
+    Route::get('/contact', [ContactController::class, 'index'])->name('contact.index');
+    Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
+
+    if (app()->environment() === 'local') {
+        Route::get('/mailable', [ContactController::class, 'show'])->middleware('auth');
+    }
 
 // Newsletter
-Route::post('/newsletter/subscribe', [SubscriberController::class, 'store'])->name('contact.subscribe');
-Route::get('/newsletter/unsubscribe/{subscriber}', [SubscriberController::class, 'delete'])->name('contact.unsubscribe');
+    Route::post('/newsletter/subscribe', [SubscriberController::class, 'store'])->name('contact.subscribe');
+    Route::get('/newsletter/unsubscribe/{subscriber}', [SubscriberController::class, 'delete'])->name('contact.unsubscribe');
+    //routes here
+});
